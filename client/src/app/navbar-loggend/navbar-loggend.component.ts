7dacb77464da935b6825c7d4e094263afd77a043
Boaca_Madalina_Elena <madalina-elena.boaca@student.tuiasi.ro>
2023-10-17 13:20:48 +0300

    Componenta navbar conectat2

---
+++ client/src/app/navbar-loggend/navbar-loggend.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-loggend',
+  templateUrl: './navbar-loggend.component.html',
+  styleUrls: ['./navbar-loggend.component.css']
+})
+export class NavbarLoggendComponent {
+
+}
