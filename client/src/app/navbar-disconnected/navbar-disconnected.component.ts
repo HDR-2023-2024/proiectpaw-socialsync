47f7d62db18de1eda989adfa72a6dddfdf3254a4
Boaca_Madalina_Elena <madalina-elena.boaca@student.tuiasi.ro>
2023-10-19 18:52:05 +0300

    Optimizari

--- client/src/app/navbar-disconnected/navbar-disconnected.component.ts
+++ client/src/app/navbar-disconnected/navbar-disconnected.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-disconnected',
   templateUrl: './navbar-disconnected.component.html',
   styleUrls: ['./navbar-disconnected.component.css']
 })
-export class NavbarDisconnectedComponent {
+export class NavbarDisconnectedComponent implements OnInit {
+  ngOnInit(): void {
+    const searchInput = document.getElementById("search-input") as HTMLInputElement;
 
+    if (searchInput) {
+      searchInput.addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+          const inputValue = searchInput.value;
+          console.log( inputValue);
+        }
+      });
+    }
+  }
+  
 }
--- client/src/app/navbar-horizontally/navbar-horizontally.component.ts
+++ client/src/app/navbar-horizontally/navbar-horizontally.component.ts
@@ -5,47 +5,42 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar-horizontally.component.html',
   styleUrls: ['./navbar-horizontally.component.css']
 })
-export class NavbarHorizontallyComponent {
-  itemsToDisplay: any[] = [
-    { id: 1, name: 'Meniul', subgroup: ["Băuturi", "Mâncare", "Deserturi"] },
-    { id: 2, name: 'Băuturi', subgroup: ["Apa", "Sucuri", "Cafea"] },
-    { id: 3, name: 'Mâncare', subgroup: ["Pizza", "Paste", "Burgeri"] },
-    { id: 4, name: 'Deserturi', subgroup: ["Tort", "Înghețată", "Prăjituri"] },
-    { id: 5, name: 'Noutăți', subgroup: ["Evenimente", "Oferte", "Promoții"] },
-    { id: 6, name: 'Istorie', subgroup: ["Perioade istorice", "Personalități istorice", "Evenimente istorice"] },
-    { id: 7, name: 'Geografie', subgroup: ["Țări", "Capitale", "Munți"] },
-    { id: 8, name: 'Sport', subgroup: ["Fotbal", "Tenis", "Baschet"] },
-    { id: 9, name: 'Știință', subgroup: ["Astronomie", "Biologie", "Chimie"] },
-    { id: 10, name: 'Tehnologie', subgroup: ["Gadget-uri", "Software", "Inovații"] },
-  ];
+export class NavbarHorizontallyComponent implements OnInit {
+
+  ngOnInit(): void {
+    var myArr: any[] = [
+      { id: 1, name: 'Meniul', subgroup: ["Băuturi", "Mâncare", "Deserturi"] },
+      { id: 2, name: 'Băuturi', subgroup: ["Apa", "Sucuri", "Cafea"] },
+      { id: 3, name: 'Mâncare', subgroup: ["Pizza", "Paste", "Burgeri"] },
+      { id: 4, name: 'Deserturi', subgroup: ["Tort", "Înghețată", "Prăjituri"] },
+      { id: 5, name: 'Noutăți', subgroup: ["Evenimente", "Oferte", "Promoții"] },
+      { id: 6, name: 'Istorie', subgroup: ["Perioade istorice", "Personalități istorice", "Evenimente istorice"] },
+      { id: 7, name: 'Geografie', subgroup: ["Țări", "Capitale", "Munți"] },
+      { id: 8, name: 'Sport', subgroup: ["Fotbal", "Tenis", "Baschet"] },
+      { id: 9, name: 'Știință', subgroup: ["Astronomie", "Biologie", "Chimie"] },
+      { id: 10, name: 'Tehnologie', subgroup: ["Gadget-uri", "Software", "Inovații"] },
+    ];
+
+    this.itemsToDisplay = myArr;
+  }
+
+  itemsToDisplay?: any[]
 
   isVisible = true;
   isVisibleFull = false;
-  
-  selectViewFull(){
+  isTopicsVisible = true;
+  isResourcesVisible = true;
+
+  selectViewFull() {
     this.isVisibleFull = !this.isVisibleFull;
   }
 
   hideTopics() {
-    console.log("hideTopics apelat")
-    var elem = document.getElementById("topics-list");
-    if (elem != null) {
-      if (this.isVisible == true) {
-        elem.style.display = 'none';
-        this.isVisible = false;
-        var title = document.getElementById("topics-arrow");
-        if (title != null) {
-          title.innerHTML = "⮟"
-        }
-      }
-      else {
-        elem.style.display = 'block';
-        this.isVisible = true;
-        var title = document.getElementById("topics-arrow");
-        if (title != null) {
-          title.innerHTML = "⮝"
-        }
-      }
-    }
+    this.isTopicsVisible = !this.isTopicsVisible;
+    console.log(this.isTopicsVisible)
+  }
+
+  hideResources() {
+    this.isResourcesVisible = !this.isResourcesVisible;
   }
 }
