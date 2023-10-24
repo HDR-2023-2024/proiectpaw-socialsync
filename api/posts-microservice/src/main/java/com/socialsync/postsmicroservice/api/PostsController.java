29901aacb03b0b1c5832841346625c08032fcb74
constantin1215 <Constantin.Ciobanu12@yahoo.com>
2023-10-24 17:23:57 +0300

    RabbitMQ setup: config + "manual test".

--- api/posts-microservice/src/main/java/com/socialsync/postsmicroservice/api/PostsController.java
+++ api/posts-microservice/src/main/java/com/socialsync/postsmicroservice/api/PostsController.java
@@ -1,9 +1,13 @@
 package com.socialsync.postsmicroservice.api;
 
+import com.socialsync.postsmicroservice.component.RabbitMqConnectionFactoryComponent;
 import com.socialsync.postsmicroservice.pojo.Post;
 import com.socialsync.postsmicroservice.service.PostsService;
 import com.socialsync.postsmicroservice.util.exceptions.PostNotFound;
 import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +18,22 @@ import java.util.HashMap;
 @RequestMapping("api/v1/posts")
 @AllArgsConstructor
 public class PostsController {
+    @Autowired
+    private RabbitMqConnectionFactoryComponent conectionFactory;
 
     private final PostsService postsService;
+
+    private AmqpTemplate amqpTemplate;
+
+    @Bean
+    void initTemplate() {
+        this.amqpTemplate = conectionFactory.rabbitTemplate();
+    }
+
+    private void sendMessage(String msg) {
+        System.out.println(msg);
+        this.amqpTemplate.convertAndSend(conectionFactory.getExchange(), conectionFactory.getRoutingKey(), msg);
+    }
 
     @GetMapping
     public ResponseEntity<HashMap<String, Post>> fetchAllPosts() {
@@ -34,6 +52,7 @@ public class PostsController {
     @PostMapping
     public ResponseEntity<String> addPost(@RequestBody Post post) {
         postsService.addPost(post);
+        sendMessage(post.getId());
 
         return new ResponseEntity<>("Added post: " + post.getId(), HttpStatus.CREATED);
     }
