005a999f0c028387670efc202378694388ee23ec
constantin1215 <Constantin.Ciobanu12@yahoo.com>
2023-10-27 23:49:49 +0300

    Started comments microservice.

---
+++ api/comments-microservice/src/test/java/com/socialsync/commentsmicroservice/CommentsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialsync.commentsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
