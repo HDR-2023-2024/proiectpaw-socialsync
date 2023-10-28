98e1f66797277dc11ea8493a5e527f2e56153ce3
denisavok4 <denisa-maria.vocurek@student.tuiasi.ro>
2023-10-28 15:28:57 +0300

    Added full-post-page component

---
+++ client/src/app/full-post-page/full-post-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-full-post-page',
+  templateUrl: './full-post-page.component.html',
+  styleUrls: ['./full-post-page.component.css']
+})
+export class FullPostPageComponent {
+
+}
