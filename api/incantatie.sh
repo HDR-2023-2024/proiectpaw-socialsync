57d0aeef3eb61de71727a14a48646ad0f3a9ff8d
constantin1215 <Constantin.Ciobanu12@yahoo.com>
2023-11-14 20:29:33 +0200

    Query-microservice initial commit + docker compose + incantation.

---
+++ api/incantatie.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+cd posts-microservice
+./mvnw clean
+./mvnw install
+cd ..
+cd comments-microservice
+./mvnw clean
+./mvnw install
+cd ..
+cd topics-microservice
+./mvnw clean
+./mvnw install
+cd ..
+cd users-microservice
+./mvnw clean
+./mvnw install
+cd ..
+cd query-microservice
+./mvnw clean
+./mvnw install
+cd ..
+docker compose up
