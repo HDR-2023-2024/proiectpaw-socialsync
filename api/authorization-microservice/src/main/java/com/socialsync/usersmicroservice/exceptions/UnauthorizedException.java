d4949af684ca5b0c90edb817a9bd0bc0ff9d942c
Boaca_Madalina_Elena <madalina-elena.boaca@student.tuiasi.ro>
2023-12-02 11:52:49 +0200

    Securizare user

--- api/authorization-microservice/src/main/java/com/socialsync/usersmicroservice/exceptions/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.socialsync.usersmicroservice.exceptions;
-
-public class UnauthorizedException extends Exception{
-    public UnauthorizedException(String s){
-        super(s);
-    }
-}
--- api/authorization-microservice/src/main/java/com/socialsync/usersmicroservice/interfaces/AuthorizationServiceMethods.java
+++
@@ -1,5 +0,0 @@
-package com.socialsync.usersmicroservice.interfaces;
-
-public interface AuthorizationServiceMethods {
-
-}
