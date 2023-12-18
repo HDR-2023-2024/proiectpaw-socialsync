ab001a9cf750fd78b5518d48ee901d57f7928579
Vocurek Denisa-Maria <dvocurek@gmail.com>
2023-12-18 21:02:59 +0200

    update create post

--- client/src/app/create-post/create-post.component.ts
+++ client/src/app/create-post/create-post.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CreatePostService } from '../create-post.service';
+import { HttpResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+import { Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
@@ -7,12 +12,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent {
-  editorConfig = {
-    editable: true,
-    spellcheck: true,
-    height: 'auto',
-    minHeight: '300px',
-    placeholder: 'Enter text here...',
-    translate: 'no'
+  
+  @Input() data: any;
+  form?: NgForm;
+
+  editorConfig:AngularEditorConfig = {
+      editable: true,
+      spellcheck: true,
+      height: '15rem',
+      minHeight: '5rem',
+      placeholder: 'Enter text here...',
+      translate: 'no',
+      customClasses: [
+        {
+          name: "quote",
+          class: "quote",
+        },
+        {
+          name: 'redText',
+          class: 'redText'
+        },
+        {
+          name: "titleText",
+          class: "titleText",
+          tag: "h1",
+        },
+      ]
   }
+
+  title: string = '';
+  content: string = '';
+  url:string=' ';
+  photo:string=' ';
+
+  constructor(private createPostService: CreatePostService, private router:Router) {}
+
+  createPost(): void {
+    this.createPostService.addPost(this.title, this.content, this.photo).subscribe(
+      (response) => {
+          console.log('Postare creata cu succes.', response);
+          if (response instanceof HttpResponse) {
+            if (response.status === 201) {  
+              this.router.navigate(['/full-post', response.body.id]); 
+              console.log(response);
+            }
+          }
+      },
+      (error) => {
+          console.error('Eroare la creare.', error);
+      }
+    ); 
+  }
+
+  clearForm(form: NgForm): void {
+    form.resetForm();
+  }
+  
 }
