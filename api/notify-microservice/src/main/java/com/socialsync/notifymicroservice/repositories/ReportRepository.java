f802c4fc2002ae30595cc9c56747bdf17d45808e
MariaFodor <maria-gabriela.fodor@student.tuiasi.ro>
2024-01-15 23:31:39 +0200

    Testare pe comment cu JUnit si ajustari notificari

--- api/notify-microservice/src/main/java/com/socialsync/notifymicroservice/repositories/ReportRepository.java
+++ api/notify-microservice/src/main/java/com/socialsync/notifymicroservice/repositories/ReportRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReportRepository extends MongoRepository<Report, String> {
+    Optional<Report> findAllByPostIdAndUserId (String postId, String userId); // nu se poate face report de mai multe ori la o postare
 }
